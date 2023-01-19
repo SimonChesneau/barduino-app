44be3b5a517a26be76dc815f0709056e021c76b1
SimonChesneau <99016191+SimonChesneau@users.noreply.github.com>
2023-01-19 15:31:41 +0100

    Editing Main Activity to use the database instead of the hardcoded values

--- app/src/main/java/com/example/barduino/MainActivity.java
+++ app/src/main/java/com/example/barduino/MainActivity.java
@@ -18,13 +18,15 @@ import com.example.barduino.drink.DrinkAdapter;
 import com.google.gson.Gson;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import static android.content.ContentValues.TAG;
 
 public class MainActivity extends Activity {
 
-    private final ArrayList<Drink> drinks = new ArrayList<Drink>();
-    private final ArrayList<Bottle> bottles = new ArrayList<Bottle>();
+    private List<Drink> drinks = null;
+    private List<Bottle> bottles = null;
     private BottleList bottleList = null;
     private MyDatabase myDatabase= null;
 
@@ -82,32 +84,102 @@ public class MainActivity extends Activity {
     }
 
     private void setUpDrinks(){
-        drinks.add(new Drink("Bloody mary",new String[]{"vodka", "tomato juice", "lemon juice"},new Double[]{4.5,9.0,1.5},R.drawable.cocktail));
+        /*drinks.add(new Drink("Bloody mary",new String[]{"vodka", "tomato juice", "lemon juice"},new Double[]{4.5,9.0,1.5},R.drawable.cocktail));
         drinks.add(new Drink("Cocktail",new String[]{"orange", "pomme", "raisin"}, new Double[]{5.0,3.0,4.0},R.drawable.cocktail1));
         drinks.add(new Drink("Jus d'orange",new String[]{"orange"}, new Double[]{100.},R.drawable.cocktail2));
         drinks.add(new Drink("Gin Tonic",new String[]{"gin", "tonic"}, new Double[]{7.0,14.0}, R.drawable.gintonic));
         drinks.add(new Drink("Mai Thai",new String[]{"rhum blanc", "rhum ambré","cointreau","sucre de canne","sirop d'orgeat"}, new Double[]{3.,3.,2.,1.,1.},R.drawable.maithai));
         drinks.add(new Drink("Mojito", new String[]{"rhum blanc","sucre de canne","eau gazeuse"},new Double[]{4.,2.,1.},R.drawable.mojito));
-        drinks.add(new Drink("Bloody mary",new String[]{"vodka", "tomato juice", "lemon juice"},new Double[]{4.5,9.0,1.5},R.drawable.cocktail));
-        drinks.add(new Drink("Cocktail",new String[]{"orange", "pomme", "raisin"}, new Double[]{5.0,3.0,4.0},R.drawable.cocktail1));
-        drinks.add(new Drink("Jus d'orange",new String[]{"orange"}, new Double[]{100.},R.drawable.cocktail2));
-        drinks.add(new Drink("Gin Tonic",new String[]{"gin", "tonic"}, new Double[]{7.0,14.0},R.drawable.gintonic));
-        drinks.add(new Drink("Mai Thai",new String[]{"rhum blanc", "rhum ambré","cointreau","sucre de canne","sirop d'orgeat"}, new Double[]{3.,3.,2.,1.,1.},R.drawable.maithai));
-        drinks.add(new Drink("Mojito", new String[]{"rhum blanc","sucre de canne","eau gazeuse"},new Double[]{4.,2.,1.},R.drawable.mojito));
+        */
+
+        if(myDatabase.getDrinksCount() == 0) {
+
+            List<Drink> liste = new ArrayList<>();
+
+            HashMap<Bottle, Double> bloodyMary = new HashMap<>();
+            bloodyMary.put(bottles.get(0), 4.5);
+            bloodyMary.put(bottles.get(6), 9.0);
+            bloodyMary.put(bottles.get(7), 1.5);
+            liste.add(new Drink("Bloody mary", bloodyMary, R.drawable.cocktail));
+
+            HashMap<Bottle, Double> cocktail = new HashMap<>();
+            cocktail.put(bottles.get(8), 5.0);
+            cocktail.put(bottles.get(9), 3.0);
+            cocktail.put(bottles.get(10), 4.0);
+            liste.add(new Drink("Cocktail", cocktail, R.drawable.cocktail1));
+
+            HashMap<Bottle, Double> orangeJuice = new HashMap<>();
+            orangeJuice.put(bottles.get(8), 100.);
+            liste.add(new Drink("Orange Juice", orangeJuice, R.drawable.cocktail2));
+
+            HashMap<Bottle, Double> ginTonic = new HashMap<>();
+            ginTonic.put(bottles.get(5), 7.0);
+            ginTonic.put(bottles.get(11), 14.0);
+            liste.add(new Drink("Gin Tonic", ginTonic, R.drawable.gintonic));
+
+            HashMap<Bottle, Double> maiThai = new HashMap<>();
+            maiThai.put(bottles.get(12), 3.);
+            maiThai.put(bottles.get(13), 3.);
+            maiThai.put(bottles.get(14), 2.);
+            maiThai.put(bottles.get(15), 1.);
+            maiThai.put(bottles.get(16), 1.);
+            liste.add(new Drink("Mai Thai", maiThai, R.drawable.maithai));
+
+            HashMap<Bottle, Double> mojito = new HashMap<>();
+            mojito.put(bottles.get(12), 4.);
+            mojito.put(bottles.get(15), 2.);
+            mojito.put(bottles.get(17), 1.);
+            liste.add(new Drink("Mojito", mojito, R.drawable.mojito));
+
+            List<Drink> liste2 = myDatabase.getAllDrinks();
+            for (Drink drink : liste2) {
+                Log.i(TAG, "drink name: " + drink.getName() + " -- id: " + drink.getId());
+            }
+
+            for (Drink drink : liste) {
+                myDatabase.insertDrinkInDatabase(drink);
+            }
+        }
+
+        drinks = myDatabase.getAllDrinks();
+        /*for(Drink drink : liste2){
+            Log.i(TAG, "drink name: "+drink.getName() + " -- id: " + drink.getId());
+        }*/
+
 
     }
 
     private void setUpBottles(){
-        bottles.add(new Bottle("Vodka", 1,R.drawable.bottle_vodka));
-        bottles.add(new Bottle("Jager", 2,R.drawable.bottle_jager));
-        bottles.add(new Bottle("Rum", 3,R.drawable.bottle_rum));
-        bottles.add(new Bottle("Tequila", 4,R.drawable.bottle_champagne));
-        bottles.add(new Bottle("Whiskey", 5,R.drawable.bottle_whiskey));
-        bottles.add(new Bottle("Gin", 6,R.drawable.bottle_gin));
-        bottleList= new BottleList(bottles);
 
-        for(Bottle bottle : bottles){
-            myDatabase.insertBottleInDatabase(bottle);
+        if(myDatabase.getBottlesCount() == 0) {
+            //Hardcoded values (now in the database)
+            bottles = new ArrayList<>();
+            bottles.add(new Bottle("Vodka", 1, R.drawable.bottle_vodka));
+            bottles.add(new Bottle("Jager", 2, R.drawable.bottle_jager));
+            bottles.add(new Bottle("Rum", 3, R.drawable.bottle_rum));
+            bottles.add(new Bottle("Tequila", 4, R.drawable.bottle_champagne));
+            bottles.add(new Bottle("Whiskey", 5, R.drawable.bottle_whiskey));
+            bottles.add(new Bottle("Gin", 6, R.drawable.bottle_gin));
+            bottles.add(new Bottle("Tomato Juice", 7, R.drawable.bottle_jager));
+            bottles.add(new Bottle("Lemon Juice", 8, R.drawable.bottle_jager));
+            bottles.add(new Bottle("Orange Juice", 9, R.drawable.bottle_jager));
+            bottles.add(new Bottle("Apple Juice", 10, R.drawable.bottle_jager));
+            bottles.add(new Bottle("Grape Juice", 11, R.drawable.bottle_jager));
+            bottles.add(new Bottle("Tonic", 12, R.drawable.bottle_jager));
+            bottles.add(new Bottle("White Rum", 13, R.drawable.bottle_jager));
+            bottles.add(new Bottle("Dark Rum", 14, R.drawable.bottle_jager));
+            bottles.add(new Bottle("Cointreau", 15, R.drawable.bottle_jager));
+            bottles.add(new Bottle("Brown Sugar", 16, R.drawable.bottle_jager));
+            bottles.add(new Bottle("Orgeat Sirop", 17, R.drawable.bottle_jager));
+            bottles.add(new Bottle("Sparkling Water", 18, R.drawable.bottle_jager));
+            for (Bottle bottle : bottles) {
+                myDatabase.insertBottleInDatabase(bottle);
+            }
         }
+
+
+
+        bottles = myDatabase.getAllBottles();
+        bottleList = new BottleList(bottles);
     }
 }
